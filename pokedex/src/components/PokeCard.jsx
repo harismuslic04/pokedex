c6a7f8ef5ddcf6779e80c5b89cf36f954c6a1084
harismuslic04 <harismuslic16@gmail.com>
2024-11-05 18:43:06 +0100

    a

--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import React from "react";
-import { getPokedexNumber } from "../utils";
-
+import { getFullPokedexNumber, getPokedexNumber } from "../utils";
+import TypeCard from "./TypeCard";
 export default function PokeCard(props) {
   const { selectedPokemon } = props;
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const { name, height, abilities, stats, types, moves, sprites } = data || {};
+
+  const imgList = Objest.keys(sprites || {}).filter((val) => {
+    if (!sprites[val]) {
+      return false;
+    }
+    if (["version", "other"].includes(val)) {
+      return false;
+    }
+    return true;
+  });
 
   useEffect(() => {
     // if loading, exit logic
@@ -52,5 +64,36 @@ export default function PokeCard(props) {
     //if we fetch from api,make sure to save the information to the cashe for next time
   }, [selectedPokemon]);
 
-  return <div></div>;
+  if (loading || !data) {
+    return (
+      <div>
+        <h4>Loading...</h4>
+      </div>
+    );
+  }
+
+  return (
+    <div className="poke-card">
+      <div>
+        <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
+        <h2>{name}</h2>
+      </div>
+      <div className="type-container">
+        {types.map((typeObj, typeIndex) => {
+          return <TypeCard key={typeIndex} type={typeObj?.type?.name} />;
+        })}
+      </div>
+      <img
+        src={"/pokemon/" + getFullPokedexNumber(selectedPokemon) + ".png"}
+        alt={`${name}-large-img`}
+      />
+      <div className="img-container">
+        {imgList.map((spriteUrl, spriteIndex) => {
+          return (
+            <img key={spriteIndex} src={""} alt={`${name}-img-${spriteUrl}`} />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- pokedex/src/components/TypeCard.jsx
+++ pokedex/src/components/TypeCard.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { pokemonTypeColors } from "../utils";
 
-export default function TypeCard() {
-  return <div>TypeCard</div>;
+export default function TypeCard(props) {
+  const { type } = props;
+  return (
+    <div
+      className="type-tile"
+      style={{
+        color: pokemonTypeColors?.[type]?.color,
+        background: pokemonTypeColors?.[type]?.background,
+      }}
+    >
+      <p>{type}</p>
+    </div>
+  );
 }
